271ebe287b7f845f5ba8d3424a648ded89010a22
wilson.calixto <wilsoncalisto2012@gmail.com>
2026-01-06 10:03:04 -0300

    feat: melhorando a responsividade

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -8,14 +8,11 @@ interface AuthLayoutProps {
 export default function LoginLayout({ children }: AuthLayoutProps) {
     return (
         <div className="flex flex-col md:flex-row w-full min-h-screen overflow-x-hidden">
-           
-                 
-
-            <div className="w-[60vw] h-screen relative">
+            <div className="w-full md:w-1/2 flex-shrink-0">
                 <Banner />
             </div>
             
-            <div className="flex items-center justify-center w-[40vw] h-screen">
+            <div className="w-full md:w-1/2 flex flex-col bg-white overflow-y-auto justify-center">
                 <div className="w-full flex flex-col items-center flex-shrink-0 px-4 py-8">
                     {children}
                 </div>
