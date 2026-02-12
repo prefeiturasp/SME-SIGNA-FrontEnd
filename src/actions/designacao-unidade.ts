40f16ca223dea7db96231c6969c9c88e5f394df5
wilson.calixto <wilsoncalisto2012@gmail.com>
2026-02-12 10:00:59 -0300

    fix: ajustando o get de designação

--- src/actions/designacao-unidade.ts
+++ src/actions/designacao-unidade.ts
@@ -3,13 +3,20 @@
 import axios, { AxiosError } from "axios";
 import { DesignacaoUnidadeResponse, DesignacaoUnidadeResult } from "@/types/designacao-unidade"; 
 import { ErrorResponse } from "@/types/generic";
+import { cookies } from "next/headers";
 
 export async function getDesignacaoUnidadeAction(codigo_ue:string): Promise<DesignacaoUnidadeResult> {
     const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 
+    const cookieStore = await cookies();
+    const authToken = cookieStore.get("auth_token")?.value;
+  
     try {
         const { data } = await axios.get<DesignacaoUnidadeResponse>(`${API_URL}/designacao/unidade`, {
             params: { codigo_ue },
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
         });
         return { success: true, data: data };
 
