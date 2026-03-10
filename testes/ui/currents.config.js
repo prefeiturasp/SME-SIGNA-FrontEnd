ca3373976ba2ef2a486f597b0dff55a7b522aa37
Wilson Santos <wilson.santos@spassu.com.br>
2026-03-10 15:59:37 -0300

    Ajuste para configurar o projeto no Jenkins

--- testes/ui/currents.config.js
+++ testes/ui/currents.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  projectId: '***REMOVED***',
-  authToken: process.env.CURRENTS_KEY || '',
-  recordKey: process.env.CURRENTS_RECORD_KEY || '',
-  cloudServiceUrl: 'https://cy.currents.dev',
+  projectId: "SME-SIGNA", // the projectId, can be any values for sorry-cypress users
+  recordKey: "somekey", // the record key, can be any value for sorry-cypress users
+  cloudServiceUrl: "http://10.50.1.202:1234",   // Sorry Cypress users - set the director service URL
   
   // CI Build ID
   ciBuildId: process.env.CI_BUILD_ID || `local-${Date.now()}`,
