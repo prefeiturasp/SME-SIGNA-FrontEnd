8ad8c4dbcca858fe0d476055a74b7971c014a3d4
wilson.calixto <wilsoncalisto2012@gmail.com>
2026-05-27 15:09:20 -0300

    fix: resolvendo erro de sonar

--- src/app/pages/baixar-lauda/page.tsx
+++ src/app/pages/baixar-lauda/page.tsx
@@ -1,43 +1,32 @@
 "use client";
-import FundoBranco from "@/components/dashboard/FundoBranco/QuadroBranco";
-import PageHeader from "@/components/dashboard/PageHeader/PageHeader";
+
+
+import { usePortariasDO } from "../../../hooks/usePortariasDO";
 import FiltroDeDo from "@/components/dashboard/Designacao/FiltroDeDo/FiltroDeDo";
+import PageHeader from "@/components/dashboard/PageHeader/PageHeader";
+import FBranco from "@/components/dashboard/FundoBranco/QuadroBranco";
+import { PORTARIAS_SEM_DATA_DE_PUBLICACAO } from "@/components/dashboard/Designacao/MainDOForm/MainDOForm";
 import { FormProvider } from "react-hook-form";
+import ListagemDeDo from "@/components/dashboard/Designacao/ListagemDeDo/ListagemDeDo";
 import {
   ListagemPortariasResponse,
 } from "@/types/designacao";
-import ListagemDeDo from "@/components/dashboard/Designacao/ListagemDeDo/ListagemDeDo";
-import  { PORTARIAS_SEM_DATA_DE_PUBLICACAO } from "@/components/dashboard/Designacao/MainDOForm/MainDOForm";
-import { message } from "antd";
-import { usePortariasDO } from "../../../hooks/usePortariasDO";
 
 export default function BaixarLauda() {
   const {
+    handleClear,
+    isPending,
+    tabelaKey,
     resultado,
     filterForm,
     onSubmitFilterForm,
-    handleClear,
-    isPending,
     salvando,
-    setSalvando,
-    tabelaKey,
   } = usePortariasDO();
 
 
 
   const handleBaixarLauda = async (selectedRows: ListagemPortariasResponse[], tipoArquivo: string) => {
     console.log('selectedRows', selectedRows, tipoArquivo);
-    try {
-      setSalvando(true);
-      message.loading({ content: "Salvando portaria...", duration: 0 });
-      message.destroy();
-
-      setSalvando(false);
-    } catch (error) {
-      console.error("Erro ao salvar portaria:", error);
-      message.destroy();
-      setSalvando(false);
-    }
   };
 
 
@@ -45,32 +34,40 @@ export default function BaixarLauda() {
   return (
     <>
       <PageHeader
-        title={"Baixar lauda"}
-        breadcrumbs={[{ title: "Início", href: "/" }, { title: "Designações", href: "/pages/listagem-designacoes" }, { title: "Baixar lauda" }]}
         showBackButton={false}
+        title={
+          "Baixar lauda"
+        }
+        breadcrumbs={[
+          { title: "Início", href: "/" },
+          { title: "Designações", href: "/pages/listagem-designacoes" },
+          { title: "Baixar lauda" }
+        ]}        
       />
 
-      <FundoBranco className="mb-4">
+      <FBranco className="mb-4">
         <FormProvider {...filterForm}>
           <form onSubmit={filterForm.handleSubmit(onSubmitFilterForm)}>
             <FiltroDeDo onClear={handleClear} />
           </form>
         </FormProvider>
-      </FundoBranco>
-      <FundoBranco className="mb-4">
+      </FBranco>
 
-      <ListagemDeDo
-          key={tabelaKey}
-          isDisabled={salvando}
-          data_considerada_portaria={ new Date()}
-          data_publicacao={new Date()}
-          value={PORTARIAS_SEM_DATA_DE_PUBLICACAO}
-          data={resultado ?? []}
-          isLoading={isPending}          
-          onClickBaixarLauda={handleBaixarLauda}                    
+      <FBranco className="mb-4">
+
+        <ListagemDeDo
           isListagemDo={false}
+          onClickBaixarLauda={handleBaixarLauda}
+          isLoading={isPending}
+          data={resultado ?? []}              
+          value={PORTARIAS_SEM_DATA_DE_PUBLICACAO}          
+          data_considerada_portaria={new Date()}          
+          isDisabled={salvando}
+          data_publicacao={new Date()}
+          key={tabelaKey}
         />
-      </FundoBranco>
+        
+      </FBranco>
     </>
   );
 }
