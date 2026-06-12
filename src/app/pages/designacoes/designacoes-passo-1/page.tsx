600dc59926ab1d1489a881fcfa863807646747e7
MarceloAlves7 <alvesmarcelinho7@gmail.com>
2026-06-12 14:45:54 -0300

    fix: :bug: Corrige sintaxe no campo categoria

--- src/app/pages/designacoes/designacoes-passo-1/page.tsx
+++ src/app/pages/designacoes/designacoes-passo-1/page.tsx
@@ -64,7 +64,7 @@ export default function DesignacoesPasso1() {
         nome_civil: data.nome_civil,
         categoria: data.categoria ?? "",
       },
-    });
+    }));
   }
 
   const onProximo = () => {
